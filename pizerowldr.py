b8c8071ddc09158598a95bb5c339baf67ced6a08
Rahul Taiwala <rktaiwala@gmail.com>
2018-03-25 13:54:14 +0530

    Update pizerowldr.py

--- pizerowldr.py
+++ pizerowldr.py
@@ -29,20 +29,15 @@ def my_publish_callback(envelope, status):
         # because of which request did fail.
         # Request can be resent using: [status retry];
  
- 
+while True: 
 class MySubscribeCallback(SubscribeCallback):
-    def presence(self, pubnub, presence):
-        pass  # handle incoming presence data
- 
+   
     def status(self, pubnub, status):
-        if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
-            pass  # This event happens when radio / connectivity is lost
- 
-        elif status.category == PNStatusCategory.PNConnectedCategory:
+        if status.category == PNStatusCategory.PNConnectedCategory:
             # Connect event. You can do stuff like publish, and know you'll get it.
             # Or just use the connected event to confirm you are subscribed for
             # UI / internal notifications, etc
-            while True:
+            
                 light = GPIO.input(pin)
                 if light==0:
                   print 'Light intensity is high'
@@ -52,18 +47,6 @@ class MySubscribeCallback(SubscribeCallback):
                                                 ['x', time.time()],
                                                 ['light_intensity', light]
                                                 ]).async(my_publish_callback)
-        elif status.category == PNStatusCategory.PNReconnectedCategory:
-            pass
-            # Happens as part of our regular operation. This event happens when
-            # radio / connectivity is lost, then regained.
-        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
-            pass
-            # Handle message decryption error. Probably client configured to
-            # encrypt messages and on live data feed it received plain text.
- 
-    def message(self, pubnub, message):
-        pass  # Handle new message stored in message.message
- 
  
 pubnub.add_listener(MySubscribeCallback())
 pubnub.subscribe().channels(channel).execute()
