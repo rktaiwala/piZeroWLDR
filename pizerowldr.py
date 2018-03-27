5192159d9884feea685a551942f6fc17c7265bed
Rahul Taiwala <rktaiwala@gmail.com>
2018-03-27 18:45:50 +0530

    Update pizerowldr.py

--- pizerowldr.py
+++ pizerowldr.py
@@ -51,7 +51,8 @@ def timeCheck(hr=16, mins=59, sec=55, micros=0):
    tom8am = tom.replace(hour=8, minute=0, second=0, microsecond=0)
    today5pm = now.replace(hour=hr, minute=mins, second=sec, microsecond=micros)
    showDebug('Time check 3')
-   showDebug('Time check %s ' % now > today5pm and now<today8am)
+   chk = now > today5pm and now<today8am
+   showDebug('Time check %s' % chk)
    return now > today5pm and now<today8am
     
 def blynkProjects():
